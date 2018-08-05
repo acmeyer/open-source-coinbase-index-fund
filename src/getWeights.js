e060e7052bdb7303a0e6ac4176b17c168d6b38f9
Craig Mulligan <hobochildster@gmail.com>
2018-08-05 19:04:51 +0100

    add prettierrc for prettier users

--- src/getWeights.js
+++ src/getWeights.js
@@ -1,33 +1,28 @@
-import rp from 'request-promise'
-import { currencyMapper } from './utils/currencyMapper'
-import { fiatCurrency } from './env'
+import rp from "request-promise";
+import { currencyMapper } from "./utils/currencyMapper";
+import { fiatCurrency } from "./env";
 
 // https://index-am.coinbase.com//v2/indices/cbi/composition.json
 // returns [{"name":"Bitcoin","abbr":"btc","value":67.72362223258995},{"name":"Ethereum","abbr":"eth","value":23.147138703100914},{"name":"Bitcoin Cash","abbr":"bch","value":6.759463195929994},{"name":"Litecoin","abbr":"ltc","value":2.369775868379143}]
 
 const options = {
-  uri: 'https://index-am.coinbase.com//v2/indices/cbi/composition.json',
-  json: true,
-}
+  uri: "https://index-am.coinbase.com//v2/indices/cbi/composition.json",
+  json: true
+};
 
 function getWeights() {
   return rp(options)
     .then(data => {
       return data.map(({ name, value }) => ({
         productId: `${currencyMapper[name]}-${fiatCurrency}`,
-        amount: parseFloat(value) / 100,
-      }))
+        amount: parseFloat(value) / 100
+      }));
     })
     .catch(err => {
-      console.log(err)
-    })
+      console.log(err);
+    });
 }
-
-;(async () => {
-  const weights = await getWeights()
-  console.log({ weights })
-})()
 
 module.exports = {
-  getWeights,
-}
+  getWeights
+};
