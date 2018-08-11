6cb11eff35a0bb56c4b04f85c57447a8b18b15f2
Alex Meyer <acmeyer9@gmail.com>
2018-08-10 20:10:19 -0400

    updated the index composition url

--- src/utils/coinbaseWeights.js
+++ src/utils/coinbaseWeights.js
@@ -1,11 +1,13 @@
 import rp from "request-promise";
 import { currencyMapper } from "./currencyMapper";
 
-// https://index-am.coinbase.com//v2/indices/cbi/composition.json
+const cbiCompositionUrl = "https://index-am.coinbase.com//v1/cbi/composition.json";
+
+// https://index-am.coinbase.com//v1/cbi/composition.json
 // returns [{"name":"Bitcoin","abbr":"btc","value":67.72362223258995},{"name":"Ethereum","abbr":"eth","value":23.147138703100914},{"name":"Bitcoin Cash","abbr":"bch","value":6.759463195929994},{"name":"Litecoin","abbr":"ltc","value":2.369775868379143}]
 
 const options = {
-  uri: "https://index-am.coinbase.com//v2/indices/cbi/composition.json",
+  uri: cbiCompositionUrl,
   json: true
 };
 
